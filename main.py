3e19d8762535fa9661c5f5e1a65ea6545499e7b1
ivasx <ambroziak.v.ivan@gmail.com>
2025-02-18 14:29:06 +0200

    Update Task1

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 def greet_message(name, surname):
-    name = name.lower().capitalize()
-    surname = surname.lower().capitalize()
-
+    name = validation(name)
+    surname = validation(surname)
     print(f"Hello world! My name is {name} {surname}")
-def validation(word):
-    if not word or not any(char.isalpha() for char in word):
 
+def validation(word):
+    while not word.strip() or not any(char.isalpha() for char in word):
+        word = input(f"The entered data ('{word}') is empty or does not contain any letters. Try again: ")
+    while not all(char.isalpha() or char == ' ' or char == '-' for char in word):
+        word = input(f"The entered data ('{word}') contains invalid characters. Only letters, spaces, and hyphens are allowed. Try again: ")
+    
+    if ' ' in word:
+        word = ' '.join([part.capitalize() for part in word.split()])
+    elif '-' in word:
+        word = '-'.join([part.capitalize() for part in word.split('-')])
+    else:
+        word = word.capitalize()
+        
+    return word
 
 def main():
     name = input("Enter name: ")
     surname = input("Enter surname: ")
+
     greet_message(name, surname)
 
 main()
