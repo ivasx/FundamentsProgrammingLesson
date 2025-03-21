90780f67ba189977ff68244ff3337c7fd7444fe4
ivasx <ambroziak.v.ivan@gmail.com>
2025-03-21 12:58:54 +0200

    void Task2.py

--- Task2.py
+++ Task2.py
@@ -1,16 +1,1 @@
-def show_console(function):
-   def wrapper(width, height):
-       result = function(width, height)
-       print(f"Площа прямокутника: {result}")
-       return result
-   return wrapper
 
-@show_console
-def get_rectangle_area(width, height):
-   area = width * height
-   return area
-
-if __name__ == '__main__':
-   width = int(input("Введіть ширину: "))
-   height = int(input("Введіть висоту: "))
-   get_rectangle_area(width, height)
