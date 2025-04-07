daf8f69aeddd38c43cf7c319e0ff435b11a954e4
ivasx <ambroziak.v.ivan@gmail.com>
2025-04-07 22:26:23 +0300

    Create Task2.py

--- Task2.py
+++ Task2.py
@@ -1,1 +1,12 @@
+def greet_students(greeting, *names):
+   if not names:
+       print("Немає студентів для привітання")
+       return
 
+
+   for name in names:
+       print(f"{greeting} {name}.")
+   return
+
+
+greet_students("Вітаю!", "Івас", "Лалала", "Оп оп оп")
