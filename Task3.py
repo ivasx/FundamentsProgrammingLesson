df93afd1bdd4f0e8c29e7c702087199ec920c2f7
ivasx <ambroziak.v.ivan@gmail.com>
2025-04-07 22:27:01 +0300

    Create Task3.py

--- Task3.py
+++ Task3.py
@@ -1,1 +1,12 @@
+def print_student_info(**kwargs):
+  if not kwargs:
+      print("Немає переданих аргументів функції.")
+  for key, value in kwargs.items():
+      print(f'{key.capitalize()} = {value}')
 
+
+
+
+print_student_info()
+print_student_info(імя = "Івас", прізвище = "Амброзяк")
+print_student_info(студент = "Опа")
