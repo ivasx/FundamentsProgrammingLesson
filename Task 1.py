cfc98ad0e95bdbebfd08aba3b75ce8362924b1f9
ivasx <ambroziak.v.ivan@gmil.com>
2025-04-28 08:35:11 +0300

    Class Clock created

---
+++ Task 1.py
@@ -0,0 +1,23 @@
+class Clock:
+    def __init__(self, time = 0):
+        self.set_time(time)
+
+    def set_time(self, time):
+        if self.__check_time(time):
+            self.__time = time
+        else:
+            self.__time = 0
+
+
+    def get_time(self):
+        return self.__time
+
+    def __check_time(self, time):
+        if isinstance(time, int) and 0 <= time < 100000:
+            return True
+        else:
+            return False
+
+if __name__ == '__main__':
+    clock = Clock(4530)
+    print(clock.get_time())
