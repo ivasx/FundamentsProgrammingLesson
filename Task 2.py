01b37a385eb444415db41ae80224c71a34738269
ivasx <ambroziak.v.ivan@gmil.com>
2025-04-28 08:47:08 +0300

    Class Money created

---
+++ Task 2.py
@@ -0,0 +1,29 @@
+class Money:
+    def __init__(self, money):
+        self.set_money(money)
+
+    def set_money(self, money):
+        if self.__check_money(money):
+            self.__money = money
+        else:
+            self.__money = 0
+
+    def get_money(self):
+        return self.__money
+
+    def add_money(self, money):
+        self.__money = self.__money + Money.get_money(money)
+
+    def __check_money(self, money):
+        if isinstance(money, int) and money >= 0:
+            return True
+        else:
+            return False
+
+if __name__ == '__main__':
+    money1 = Money(10)
+    money2 = Money(20)
+    money1.set_money(100)
+    money2.add_money(money1)
+    print(money1.get_money())  # Output: 100
+    print(money2.get_money())  # Output: 120
