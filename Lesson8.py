a9d51ccfbe1f210ba3dbd016df7f1a3700f35777
ivasx <ambroziak.v.ivan@gmil.com>
2025-06-01 20:35:59 +0300

    Lesson 8

---
+++ Lesson8.py
@@ -0,0 +1,15 @@
+print(4 > 2)
+print(4 < 2)
+print(type(1))
+print(type(True))
+
+x = int(input("Введіть чиcло: "))
+if x % 2 == 0:
+    print("Число парне")
+else:
+    print("Число не парне")
+
+y = 1.85
+
+print(y >= -2 and y <= 5)
+print(bool(""))
